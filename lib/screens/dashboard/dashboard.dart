1eaed5dcc6bc18046b8b23eda109985c306149eb
Nattapoom Petchana <pond_nattapoom@hotmail.com>
2022-09-13 02:23:23 +0700

    create dashboard screen

---
+++ lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+  static const String id = 'Dashboard';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text("hello"),
+    ));
+  }
+}
