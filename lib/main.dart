32fa75fcaa1a0be119eafe2d2f37a43ff3d3c697
Nattapoom Petchana <pond_nattapoom@hotmail.com>
2022-09-13 02:24:33 +0700

    modify new main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,30 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart';
+
+import 'router/router.dart';
+import 'screens/index.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// ส่วนของ Stateless widget
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sensors Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(
+          secondary: Colors.purple,
+        ),
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.red)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'First Flutter App',
+      initialRoute: Launcher.id, // สามารถใช้ home แทนได้
+      routes: AppRouter.routeScreens,
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const int _snakeRows = 20;
-  static const int _snakeColumns = 20;
-  static const double _snakeCellSize = 10.0;
-
-  List<double>? _accelerometerValues;
-  List<double>? _userAccelerometerValues;
-  List<double>? _gyroscopeValues;
-  List<double>? _magnetometerValues;
-  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
-  @override
-  Widget build(BuildContext context) {
-    final accelerometer =
-        _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final gyroscope =
-        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final userAccelerometer = _userAccelerometerValues
-        ?.map((double v) => v.toStringAsFixed(1))
-        .toList();
-    final magnetometer =
-        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sensor Example'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Center(
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: Colors.black38),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Accelerometer: $accelerometer'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('UserAccelerometer: $userAccelerometer'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Gyroscope: $gyroscope'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Magnetometer: $magnetometer'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    for (final subscription in _streamSubscriptions) {
-      subscription.cancel();
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(const Duration(seconds: 1), (_) {
-      _streamSubscriptions.add(
-        accelerometerEvents.listen(
-          (AccelerometerEvent event) {
-            setState(() {
-              _accelerometerValues = <double>[event.x, event.y, event.z];
-              // print(_accelerometerValues);
-            });
-          },
-        ),
-      );
-      _streamSubscriptions.add(
-        gyroscopeEvents.listen(
-          (GyroscopeEvent event) {
-            setState(() {
-              _gyroscopeValues = <double>[event.x, event.y, event.z];
-            });
-          },
-        ),
-      );
-      _streamSubscriptions.add(
-        userAccelerometerEvents.listen(
-          (UserAccelerometerEvent event) {
-            setState(() {
-              _userAccelerometerValues = <double>[event.x, event.y, event.z];
-            });
-          },
-        ),
-      );
-      print(_accelerometerValues);
-      print(_gyroscopeValues);
-      print(_userAccelerometerValues);
-    });
   }
 }
