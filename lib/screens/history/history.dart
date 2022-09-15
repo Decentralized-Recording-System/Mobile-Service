7c00b1e2d16b48c78f25fcc23e79058bb2c2f5d5
Nattapoom Petchana <pond_nattapoom@hotmail.com>
2022-09-15 15:20:00 +0700

    [2][26] create history screen

--- lib/screens/history/history.dart
+++ lib/screens/history/history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -8,11 +9,54 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
+  final controller = PageController(viewportFraction: 0.8, keepPage: true);
   @override
   Widget build(BuildContext context) {
+    final pages = List.generate(
+        8,
+        (index) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.grey.shade300,
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              child: Container(
+                height: 280,
+                child: Center(
+                    child: Text(
+                  "Page $index",
+                  style: TextStyle(color: Colors.indigo),
+                )),
+              ),
+            ));
     return Scaffold(
-        body: Center(
-      child: Text('history'),
+        body: Column(
+      children: [
+        SizedBox(height: 50),
+        SizedBox(
+          height: 240,
+          child: PageView.builder(
+            controller: controller,
+            // itemCount: pages.length,
+            itemBuilder: (_, index) {
+              return pages[index % pages.length];
+            },
+          ),
+        ),
+        SizedBox(height: 10),
+        SmoothPageIndicator(
+            controller: controller,
+            count: pages.length,
+            effect: ScrollingDotsEffect(
+              activeStrokeWidth: 2.6,
+              activeDotScale: 1.3,
+              maxVisibleDots: 5,
+              radius: 8,
+              spacing: 10,
+              dotHeight: 12,
+              dotWidth: 12,
+            )),
+      ],
     ));
   }
 }
