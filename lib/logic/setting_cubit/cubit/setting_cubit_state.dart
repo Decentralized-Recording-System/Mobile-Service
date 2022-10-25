800e543bef953c488039d9c1ea8c3dd72fb575d4
Nattapoom Petchana <pond_nattapoom@hotmail.com>
2022-10-25 22:40:22 +0700

    [6][76] create language cubit

---
+++ lib/logic/setting_cubit/cubit/setting_cubit_state.dart
@@ -0,0 +1,25 @@
+part of 'setting_cubit_cubit.dart';
+
+@immutable
+abstract class SettingCubitState {
+  const SettingCubitState();
+
+  List<Object> get props => [];
+}
+
+class SettingCubitInitial extends SettingCubitState {}
+
+class StateLanguages extends SettingCubitState {
+  final Locale locale;
+  const StateLanguages({required this.locale});
+  @override
+  List<Object> get props => [locale];
+}
+
+class StateTheme extends SettingCubitState {
+  final bool isDarkTheme;
+  final Locale locale;
+  const StateTheme({required this.isDarkTheme, required this.locale});
+  @override
+  List<Object> get props => [isDarkTheme];
+}
